8b89cac09fd690686fd48ac21b4948cb5143c706
hyy <afxhyy@163.com>
2019-04-18 15:29:22 +0800

    倍数

---
+++ ETH/Classes/Module/ChessCard(棋牌娱乐)/3DGame( 游戏)/ETHMultipleTableCell.h
@@ -0,0 +1,20 @@
+//
+//  ETHMultipleTableCell.h
+//  ETH
+//
+//  Created by admin on 2019/4/18.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ 倍数cell
+ */
+@interface ETHMultipleTableCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
